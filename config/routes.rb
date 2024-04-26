9bc83ca0c419a5026dcf43aad0d92e6bfa07b37b
Michael Gauthier <mikegauthier222@hotmail.com>
2024-04-26 12:50:17 -0700

    add programing language scaffold and seed sb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  root "articles#index"
-  
-  get "/articles", to: "articles#index"
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'programming_languages#index'
+  resources :programming_languages
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Clear existing data
+ProgrammingLanguage.destroy_all
+
+# Seed data
+ProgrammingLanguage.create!(
+  title: "Ruby",
+  description: "Ruby is a dynamic, reflective, object-oriented, general-purpose programming language.",
+  link: "https://www.ruby-lang.org/"
+)
+
+ProgrammingLanguage.create!(
+  title: "Python",
+  description: "Python is an interpreted, high-level, general-purpose programming language.",
+  link: "https://www.python.org/"
+)
+
+ProgrammingLanguage.create!(
+  title: "JavaScript",
+  description: "JavaScript is a high-level, interpreted programming language.",
+  link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+)
+
+# Add more seed data as needed
